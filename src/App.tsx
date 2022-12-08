78908625695c71cc4348de894468ca90b275779a
Shyam Sunder Verma <229291+ssv445@users.noreply.github.com>
2022-12-08 09:13:25 +0000

    functional app without redux

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ import { Layout } from 'antd';
 import './App.css';
 import { Card,  Col, Row, List } from 'antd';
 import {Pagination} from 'antd'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import {  Modal } from 'antd';
+import { useSearchParams } from "react-router-dom";
+
+
 const { Header, Footer, Sider, Content } = Layout;
 
 export interface IProduct {
@@ -24,35 +32,111 @@ export interface IPagination{
   page: number
 }
 
-function Products({ products, pagination }: {products:IProduct[], pagination:IPagination}) {
+function ProductsPage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = parseInt(searchParams.get('page')|| '1');
+  const limit = 6;
+  const selectedCategory = searchParams.get('category') || 'ALL';
+  const productId = parseInt(searchParams.get('productId') || '0') ;
+  const [products, setProducts] = useState([]);
+  const [pagination, setPagination] = useState({ total: 0, limit: 6, page: 1 });
+
+  function doProductSelect(productId) {
+    let newSearchParams = searchParams;
+    newSearchParams.set('productId',productId)
+    setSearchParams(newSearchParams);
+  }
+
+  function doSetPage(pageNo) {
+    let newSearchParams = searchParams;
+    newSearchParams.set('page',pageNo)
+    setSearchParams(newSearchParams);
+  }
+
+
+  useEffect(() => {
+    let skip = (page - 1) * limit;
+
+    let url = 'https://dummyjson.com/products/'
+    if (selectedCategory && selectedCategory !== 'ALL') {
+      url = 'https://dummyjson.com/products/category/' + selectedCategory;
+    }
+
+    fetch(`${url}?skip=${skip}&limit=${limit}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setProducts(data.products);
+        setPagination({
+          total: data.total,
+          limit: data.limit,
+          page: (data.skip / data.limit) + 1
+        })
+
+
+      });
+  }, [page, selectedCategory]);
+
+   const productModal = productId ?  <ProductModal productId={productId}/> : <div></div>
+  //const productModal = productId ?  <div>{productId}</div> : <div></div>
+
   return (
-    <div>
-    <Row gutter={8}>
-      {products.map(product => (
-        <Col span={8}>
-          <Product key={product.id} product={product} />
+    <>
+      <Sider theme="light">
+      <Categories selected={selectedCategory}></Categories>
+
+        </Sider>
+        <Content>
+      <Row gutter={8}>
+      {products.map((product:IProduct) => (
+        <Col span={8} onClick={() => { doProductSelect(product.id);  return false; }} >
+          <Product key={product.id} product={product}  />
         </Col>
       ))}
-    </Row>
+      </Row>
       <Row>
         {/* onChange=PENDING */}
-        <Pagination  defaultCurrent={pagination.page} total={pagination.total} showSizeChanger={ false} />
-    </Row>
-    </div>
+          <Pagination onChange={(page) => doSetPage(page)} defaultCurrent={pagination.page}
+            total={pagination.total}
+            showSizeChanger={false}       defaultPageSize={pagination.limit}
+
+            showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
+  />
+      </Row>
+        {productModal}
+                </Content>
+
+    </>
   );
 }
 
-function Categories({ categories, selected=null }: {categories:string[], selected:string|null}) {
+function Categories({ selected = null }: { selected: string | null }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [categories, setCategories] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products/categories`)
+      .then(response => response.json())
+      .then(data => {
+        setCategories(['ALL', ...data]);
+      });
+  }, []);
+
+   function doCategorySelect(category: string) {
+    let newSearchParams = searchParams;
+    newSearchParams.set('category',category)
+    setSearchParams(newSearchParams);
+  }
   return (
     <div>
       <List
       header={<h1>Filter By Category</h1>}
       bordered
-        dataSource={categories}
+      dataSource={categories}
         // onSelect need to change selected category
       renderItem={category => (
-        <List.Item>
-          {category}
+        <List.Item onClick={() => { doCategorySelect(category);  return false; }}>
+          {category === selected ? (<strong>{category}</strong>) : (<>{category}</>)}
         </List.Item>
       )}
     />
@@ -71,57 +155,66 @@ function Product({ product }: { product: IProduct }) {
   );
 }
 
-function ProductModal({ product }: { product: IProduct }) {
+function ProductModal({ productId }: { productId: number }) {
+  const [open, setOpen] = useState(true);
+  const [product, setProduct] = useState<IProduct>();
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products/${productId}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setProduct(data);
+      });
+        setOpen(true);
+
+  }, [productId]);
+
+  let productOrErrorCard = <div>Error</div>;
+
+    if(product){
+      productOrErrorCard = <Card hoverable
+        cover={<img height="240" alt={product.title} src={product.thumbnail} />}
+      >
+        <p>{product.title} </p>
+        <p>{product.description} </p>
+        <p>${product.price}</p>
+        <p>{product.brand} | {product.stock}</p>
+      </Card>;
+      }
   return (
-    <Card hoverable
-      cover={<img height="240" alt={product.title} src={product.thumbnail} />}
-    >
-      <p>{product.title} </p>
-      <p>{product.description} </p>
-      <p>${product.price}</p>
-      <p>{product.brand} | { product.stock}</p>
-    </Card>
+    <>
+      <Modal
+        centered
+        open={open}
+        onOk={() => setOpen(false)}
+        onCancel={() => setOpen(false)}
+      >
+        {productOrErrorCard}
+      </Modal>
+
+    </>
   );
 }
 
+const router = createBrowserRouter([
+  {
+    path: "/*",
+    element: <ProductsPage />,
+  },
+]);
+
+
 function App() {
-  const page = 1;
-  const limit = 6;
-  const [products, setProducts] = useState([]);
-  const [pagination, setPagination] = useState({ total: 0, limit: 6, page: 1 });
-  const [categories, setCategories] = useState([]);
-
-
-  useEffect(() => {
-    let skip = (page - 1) * limit;
-
-    fetch(`https://dummyjson.com/products/?skip=${skip}&limit=${limit}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data.products);
-        setProducts(data.products);
-        setPagination({
-          total: data.total,
-          limit: data.limit,
-          page: (data.skip / data.limit) + 1
-        })
-
-        let cats = data.products.map((product: IProduct) => (product.category))
-          .filter((v:string, i:number, a:string[]) => (a.indexOf(v) == i) );
-        setCategories(cats);
-      });
-  }, []);
-
   return (
     <Layout>
       <Header>Header</Header>
       <Layout>
-        <Sider         theme="light"><Categories categories={ categories} selected={null}></Categories></Sider>
-        <Content><Products products={products} pagination={pagination} /></Content>
-      </Layout>
+        <RouterProvider router={router} />
+
+        </Layout>
       <Footer>Footer</Footer>
     </Layout>
-
   );
 }
 
