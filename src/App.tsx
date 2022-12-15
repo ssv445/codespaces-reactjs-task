67eb532dce52592db213f14f072296095d434c61
Shyam Sunder Verma <229291+ssv445@users.noreply.github.com>
2022-12-15 10:29:55 +0000

    formatting from prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import { useEffect, useState } from 'react';
 import './App.css';
-import { Card,  Col, Row, List, Pagination,Modal,Layout } from 'antd';
+import { Card, Col, Row, List, Pagination, Modal, Layout } from 'antd';
 import {
   createBrowserRouter,
   RouterProvider,
-  useSearchParams
-} from "react-router-dom";
-import { useSelector, useDispatch, Provider } from 'react-redux'
-import {IStoreState, store} from './store';
+  useSearchParams,
+} from 'react-router-dom';
+import { useSelector, useDispatch, Provider } from 'react-redux';
+import { IStoreState, store } from './store';
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -18,103 +18,118 @@ export interface IProduct {
   title: string;
   description: string;
   price: number;
-  thumbnail: string,
-  category: string,
-  brand: string,
-  stock:number
+  thumbnail: string;
+  category: string;
+  brand: string;
+  stock: number;
 }
 
-export interface IPagination{
-  total: number,
-  limit: number,
-  page: number
+export interface IPagination {
+  total: number;
+  limit: number;
+  page: number;
 }
 
 function ProductsPage() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const page = parseInt(searchParams.get('page')|| '1');
+  const page = parseInt(searchParams.get('page') || '1');
   const limit = 6;
   const selectedCategory = searchParams.get('category') || 'ALL';
-  const productId = parseInt(searchParams.get('productId') || '0') ;
+  const productId = parseInt(searchParams.get('productId') || '0');
   const [pagination, setPagination] = useState({ total: 0, limit: 6, page: 1 });
-  const products = useSelector((state:IStoreState) => state.categorized[selectedCategory]?.pages[page] ?? []);
+  const products = useSelector(
+    (state: IStoreState) =>
+      state.categorized[selectedCategory]?.pages[page] ?? []
+  );
   const dispatch = useDispatch();
   // console.log('products list now is ', selectedCategory, page, products);
 
   function doProductSelect(productId) {
     let newSearchParams = searchParams;
-    newSearchParams.set('productId',productId)
+    newSearchParams.set('productId', productId);
     setSearchParams(newSearchParams);
   }
 
   function doSetPage(pageNo) {
     let newSearchParams = searchParams;
-    newSearchParams.set('page',pageNo)
+    newSearchParams.set('page', pageNo);
     setSearchParams(newSearchParams);
   }
 
-  const shouldFetchProduct = products.length === 0 ;
+  const shouldFetchProduct = products.length === 0;
   useEffect(() => {
     let skip = (page - 1) * limit;
 
-    let url = 'https://dummyjson.com/products'
-    if ( selectedCategory !== 'ALL') {
+    let url = 'https://dummyjson.com/products';
+    if (selectedCategory !== 'ALL') {
       url = 'https://dummyjson.com/products/category/' + selectedCategory;
     }
 
     if (shouldFetchProduct) {
-    // console.log('fetching product list page',page,selectedCategory, products);
+      // console.log('fetching product list page',page,selectedCategory, products);
 
-    fetch(`${url}?skip=${skip}&limit=${limit}`)
-      .then(response => response.json())
-      .then(data => {
-        //sometime page
-        dispatch({
-          type: 'ADD_CATEGORY_PRODUCT_PAGE',
-          category: selectedCategory,
-          products: data.products,
-          total: data.total,
-          per_page: data.limit,
-          page: page
+      fetch(`${url}?skip=${skip}&limit=${limit}`)
+        .then((response) => response.json())
+        .then((data) => {
+          //sometime page
+          dispatch({
+            type: 'ADD_CATEGORY_PRODUCT_PAGE',
+            category: selectedCategory,
+            products: data.products,
+            total: data.total,
+            per_page: data.limit,
+            page: page,
+          });
+
+          setPagination({
+            total: data.total,
+            limit: data.limit,
+            page: data.skip / data.limit + 1,
+          });
         });
-
-        setPagination({
-          total: data.total,
-          limit: data.limit,
-          page: (data.skip / data.limit) + 1
-        })
-      });
     }
-  }, [page, selectedCategory, dispatch,shouldFetchProduct]);
+  }, [page, selectedCategory, dispatch, shouldFetchProduct]);
 
-   const productModal = productId ?  <ProductModal productId={productId}/> : <div></div>
+  const productModal = productId ? (
+    <ProductModal productId={productId} />
+  ) : (
+    <div></div>
+  );
   //const productModal = productId ?  <div>{productId}</div> : <div></div>
 
   return (
     <>
       <Sider theme="light">
-      <Categories selected={selectedCategory}></Categories>
-
-        </Sider>
-        <Content>
-      <Row gutter={8}>
-      {products.map((product:IProduct) => (
-        <Col key={product.id} span={8} onClick={() => { doProductSelect(product.id);  return false; }} >
-          <Product key={product.id} product={product}  />
-        </Col>
-      ))}
-      </Row>
-      <Row>
-        {/* onChange=PENDING */}
-          <Pagination onChange={(page) => doSetPage(page)} defaultCurrent={pagination.page}
+        <Categories selected={selectedCategory}></Categories>
+      </Sider>
+      <Content>
+        <Row gutter={8}>
+          {products.map((product: IProduct) => (
+            <Col
+              key={product.id}
+              span={8}
+              onClick={() => {
+                doProductSelect(product.id);
+                return false;
+              }}
+            >
+              <Product key={product.id} product={product} />
+            </Col>
+          ))}
+        </Row>
+        <Row>
+          {/* onChange=PENDING */}
+          <Pagination
+            onChange={(page) => doSetPage(page)}
+            defaultCurrent={pagination.page}
             total={pagination.total}
-            showSizeChanger={false}       defaultPageSize={pagination.limit}
+            showSizeChanger={false}
+            defaultPageSize={pagination.limit}
             showTotal={(total, range) => `Total ${total} items`}
-  />
-      </Row>
+          />
+        </Row>
         {productModal}
-                </Content>
-
+      </Content>
     </>
   );
 }
@@ -122,7 +137,9 @@ function ProductsPage() {
 function Categories({ selected = null }: { selected: string | null }) {
   const [searchParams, setSearchParams] = useSearchParams();
   // const [categories, setCategories] = useState<string[]>([]);
-  const categories: string[] = useSelector((state:IStoreState) => state.categories);
+  const categories: string[] = useSelector(
+    (state: IStoreState) => state.categories
+  );
   const dispatch = useDispatch();
   //console.log('categories list now is ', categories);
   const shouldFetchCategories = categories.length === 0;
@@ -131,46 +148,57 @@ function Categories({ selected = null }: { selected: string | null }) {
     if (shouldFetchCategories) {
       console.log('fetching categories');
       fetch(`https://dummyjson.com/products/categories`)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           dispatch({
             type: 'SET_CATEGORIES',
-            categories: ['ALL', ...data]
+            categories: ['ALL', ...data],
           });
         });
     }
-  }, [shouldFetchCategories,dispatch]);
+  }, [shouldFetchCategories, dispatch]);
 
-   function doCategorySelect(category: string) {
+  function doCategorySelect(category: string) {
     let newSearchParams = searchParams;
-     newSearchParams.set('category', category)
-     newSearchParams.set('page', '1');// reset to first page
+    newSearchParams.set('category', category);
+    newSearchParams.set('page', '1'); // reset to first page
     setSearchParams(newSearchParams);
   }
   return (
     <div>
       <List
-      header={<h1>Filter By Category</h1>}
-      bordered
-      dataSource={categories}
+        header={<h1>Filter By Category</h1>}
+        bordered
+        dataSource={categories}
         // onSelect need to change selected category
-      renderItem={category => (
-        <List.Item onClick={() => { doCategorySelect(category);  return false; }}>
-          {category === selected ? (<strong>{category}</strong>) : (<>{category}</>)}
-        </List.Item>
-      )}
-    />
-
+        renderItem={(category) => (
+          <List.Item
+            onClick={() => {
+              doCategorySelect(category);
+              return false;
+            }}
+          >
+            {category === selected ? (
+              <strong>{category}</strong>
+            ) : (
+              <>{category}</>
+            )}
+          </List.Item>
+        )}
+      />
     </div>
   );
 }
 
 function Product({ product }: { product: IProduct }) {
   return (
-    <Card hoverable
+    <Card
+      hoverable
       cover={<img height="240" alt={product.title} src={product.thumbnail} />}
     >
-      <p>{product.title} | ${product.price}</p>
+      <p>
+        {product.title} | ${product.price}
+      </p>
     </Card>
   );
 }
@@ -178,7 +206,9 @@ function Product({ product }: { product: IProduct }) {
 function ProductModal({ productId }: { productId: number }) {
   const [open, setOpen] = useState(true);
   // const [product, setProduct] = useState<IProduct>();
-  const product = useSelector((state:IStoreState) => state.products[productId] ?? null);
+  const product = useSelector(
+    (state: IStoreState) => state.products[productId] ?? null
+  );
   const dispatch = useDispatch();
   // console.log('checking product info',productId, product);
   const shouldFetchProduct = !product || product.id !== productId;
@@ -186,30 +216,35 @@ function ProductModal({ productId }: { productId: number }) {
     if (shouldFetchProduct) {
       // console.log('>> requesting product info',productId, product);
       fetch(`https://dummyjson.com/products/${productId}`)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           dispatch({
             type: 'ADD_PRODUCT',
-            product: data
+            product: data,
           });
         });
     }
     //open the modal
     setOpen(true);
-  }, [productId, shouldFetchProduct,dispatch]);
+  }, [productId, shouldFetchProduct, dispatch]);
 
   let productOrErrorCard = <div>Error</div>;
 
-    if(product){
-      productOrErrorCard = <Card hoverable
+  if (product) {
+    productOrErrorCard = (
+      <Card
+        hoverable
         cover={<img height="240" alt={product.title} src={product.thumbnail} />}
       >
         <p>{product.title} </p>
         <p>{product.description} </p>
         <p>${product.price}</p>
-        <p>{product.brand} | {product.stock}</p>
-      </Card>;
-      }
+        <p>
+          {product.brand} | {product.stock}
+        </p>
+      </Card>
+    );
+  }
   return (
     <>
       <Modal
@@ -220,33 +255,29 @@ function ProductModal({ productId }: { productId: number }) {
       >
         {productOrErrorCard}
       </Modal>
-
     </>
   );
 }
 
 const router = createBrowserRouter([
   {
-    path: "/*",
+    path: '/*',
     element: <ProductsPage />,
   },
 ]);
-
 
 function App() {
   return (
     <Provider store={store}>
       <Layout>
-      <Header>Header</Header>
-      <Layout>
-        <RouterProvider router={router} />
-
+        <Header>Header</Header>
+        <Layout>
+          <RouterProvider router={router} />
         </Layout>
-      <Footer>Footer</Footer>
+        <Footer>Footer</Footer>
       </Layout>
     </Provider>
   );
 }
-
 
 export default App;
--- src/store.ts
+++ src/store.ts
@@ -2,66 +2,65 @@ import { createStore } from 'redux';
 import { IProduct } from './App';
 
 type ICategoryPage = {
-    total: number,
-    per_page: number,
-    pages: IProduct[]
+  total: number;
+  per_page: number;
+  pages: IProduct[];
 };
 
 type IStoreState = {
-    products: IProduct[],
-    categorized: ICategoryPage[],
-    categories: string[],
-}
-
-const initialState:IStoreState = {
-    products: [], // product_id -> porduct
-    categorized: [], // category_id -> category products in paginated
-    categories:[],
+  products: IProduct[];
+  categorized: ICategoryPage[];
+  categories: string[];
 };
 
+const initialState: IStoreState = {
+  products: [], // product_id -> porduct
+  categorized: [], // category_id -> category products in paginated
+  categories: [],
+};
 
-function productReducer(state:IStoreState = initialState, action) {
-    let newState:IStoreState ;
+function productReducer(state: IStoreState = initialState, action) {
+  let newState: IStoreState;
   switch (action.type) {
-      case 'ADD_PRODUCT':
+    case 'ADD_PRODUCT':
+      newState = { ...state };
+      newState.products[action.product.id] = action.product;
+      console.log('store: product set');
+      return newState;
 
-          newState = { ...state };
-          newState.products[action.product.id] = action.product;
-          console.log('store: product set');
-          return newState;
+    case 'ADD_CATEGORY_PRODUCT_PAGE':
+      // add to specific category & page
+      newState = { ...state };
+      if (newState.categorized[action.category] === undefined) {
+        newState.categorized[action.category] = {
+          total: 0,
+          per_page: 0,
+          pages: [],
+        };
+      }
+      newState.categorized[action.category].total = action.total;
+      newState.categorized[action.category].per_page = action.per_page;
+      newState.categorized[action.category].pages[action.page] =
+        action.products;
 
-      case 'ADD_CATEGORY_PRODUCT_PAGE':
-          // add to specific category & page
-          newState = { ...state };
-          if (newState.categorized[action.category] === undefined) {
-              newState.categorized[action.category] = {
-                total: 0,
-                per_page: 0,
-                pages: []
-              };
-          }
-          newState.categorized[action.category].total = action.total;
-          newState.categorized[action.category].per_page = action.per_page;
-          newState.categorized[action.category].pages[action.page] = action.products;
+      //add to product index as well
+      action.products.forEach((product) => {
+        newState.products[product.id] = product;
+      });
 
-          //add to product index as well
-          action.products.forEach(product => {
-              newState.products[product.id] = product;
-          });
+      //console.log('In action ADD_CATEGORY_PRODUCT_PAGE', action, newState);
+      console.log('store: page set');
+      return newState;
 
-          //console.log('In action ADD_CATEGORY_PRODUCT_PAGE', action, newState);
-           console.log('store: page set');
-          return newState;
+    case 'SET_CATEGORIES':
+      newState = { ...state };
+      newState.categories = action.categories;
+      //update all data
+      //console.log(action,newState);
+      console.log('store: categories set');
+      return newState;
 
-      case 'SET_CATEGORIES':
-          newState = { ...state };
-          newState.categories = action.categories
-          //update all data
-          //console.log(action,newState);
-          console.log('store: categories set');
-          return newState;
-
-      default:
+    default:
       return state;
   }
 }
